887c5ec722cfe102426bdd1bc615c3c1cc4cbcf1
Yousuf-Hashmi446 <yousufhashmi446@gmail.com>
2024-08-27 12:52:34 +0500

    initial push of data-scrape app

--- scrape_data/appartments_scrape/appartments_scrape/spiders/craigslist.py
+++ scrape_data/appartments_scrape/appartments_scrape/spiders/craigslist.py
@@ -4,7 +4,8 @@ import scrapy
 class CraigslistSpider(scrapy.Spider):
     name = "craigslist"
     allowed_domains = ["dallas.craigslist.org"]
-    start_urls = ["https://dallas.craigslist.org/search/apa#search=1~gallery~1~52"]
+    start_urls = ["https://dallas.craigslist.org/search/apa#search=1~gallery~0~0"]
 
     def parse(self, response):
         pass
+
